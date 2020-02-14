3e9a71861336d1b30b3715374a2454e9773d02b4
mauxpl <56780559+mauxpl@users.noreply.github.com>
2020-02-14 14:00:09 -0500

    CSSS NEW FILE

--- wp-config.php
+++ wp-config.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 /**
  * The base configuration for WordPress
@@ -88,3 +89,95 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
+=======
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'ma_db' );
+
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'I,=.H52VrTlFszuHL9fElE5Eoj|z0A~d-p/lBT}<Uq=px-8a4[Y^%_v_O^?r;t!U' );
+define( 'SECURE_AUTH_KEY',  'R3u+p=Qm+^$}rp/@wSfJP=WCRpaUj@gNa?`:WT/=R&&]>!>/%%^[^bW$?#}AsD8[' );
+define( 'LOGGED_IN_KEY',    'k<{gQ_V0,+WG:<3S.2z}R44lIS8-dQ{});xLj;Sqs;IqSc:xf/8OELbqaXm-t<-e' );
+define( 'NONCE_KEY',        '=P}82M+CVITT &+gA%-/sJ@;~pp7M L(z.9MzQmD{r5K%@16)<SCJ:V>-;R7lpkK' );
+define( 'AUTH_SALT',        'EZ*ul8,Tk.?@x).1mqI?{oz(6m+*BsbUTv=2rYUzSAKC_Vr:Q.QY%Wp(,H;%n#[{' );
+define( 'SECURE_AUTH_SALT', 'i3IBw2gxiaOTzq-rK_0A+~S`eP?cRqY`Sy]Wtw;TO%7<h<EFT #6Aw}`N|;:|2VF' );
+define( 'LOGGED_IN_SALT',   'l+Z&5Lcj-*+W;j(&!y2y3ieG&zK9[Kz-80x_:TobMgf{+i1W9= 4mQf#<*.OnK0_' );
+define( 'NONCE_SALT',       'Eapi=Jh,nNqTE(q>s,sBL-OJ;tad;87I{>S#.+}m[HXnU~eDQ7.yx![aW!nk^[?@' );
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
+>>>>>>> e0130585e53209f885b18e72ee7f30c3014065cd
--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Acceder &lsaquo; MADESIGIN &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
+<<<<<<< HEAD
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/css/dashicons.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/css/buttons.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/forms.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/l10n.min.css?ver=5.3.2' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/login.min.css?ver=5.3.2' media='all' />
+=======
 <link rel='stylesheet' id='dashicons-css'  href='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/css/dashicons.min.css?ver=5.3.2' media='all' />
 <link rel='stylesheet' id='buttons-css'  href='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/css/buttons.min.css?ver=5.3.2' media='all' />
 <link rel='stylesheet' id='forms-css'  href='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/forms.min.css?ver=5.3.2' media='all' />
 <link rel='stylesheet' id='l10n-css'  href='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/l10n.min.css?ver=5.3.2' media='all' />
 <link rel='stylesheet' id='login-css'  href='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/css/login.min.css?ver=5.3.2' media='all' />
+>>>>>>> e0130585e53209f885b18e72ee7f30c3014065cd
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -25,7 +33,11 @@
 		<div id="login">
 		<h1><a href="https://es.wordpress.org/">Funciona gracias a WordPress</a></h1>
 	
+<<<<<<< HEAD
+		<form name="loginform" id="loginform" action="http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-login.php" method="post">
+=======
 		<form name="loginform" id="loginform" action="/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-login.php" method="post">
+>>>>>>> e0130585e53209f885b18e72ee7f30c3014065cd
 			<p>
 				<label for="user_login">Nombre de usuario o dirección de correo</label>
 				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
@@ -49,12 +61,40 @@
 		</form>
 
 					<p id="nav">
+<<<<<<< HEAD
+									<a href="http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
+=======
 									<a href="/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
+>>>>>>> e0130585e53209f885b18e72ee7f30c3014065cd
 								</p>
 					<script type="text/javascript">
 			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
 wp_attempt_focus();
 if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+<<<<<<< HEAD
+				<p id="backtoblog"><a href="http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/">
+		&larr; Volver a MADESIGIN		</a></p>
+			</div>
+	<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp'></script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1'></script>
+<script>
+var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+</script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/js/zxcvbn-async.min.js?ver=1.0'></script>
+<script>
+var pwsL10n = {"unknown":"Fortaleza de contrase\u00f1a desconocida","short":"Muy d\u00e9bil","bad":"D\u00e9bil","good":"Medio","strong":"Fuerte","mismatch":"No coinciden"};
+</script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/js/password-strength-meter.min.js?ver=5.3.2'></script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/js/underscore.min.js?ver=1.8.3'></script>
+<script>
+var _wpUtilSettings = {"ajax":{"url":"\/MADESIGNART-PROJET-2020\/wp-admin\/admin-ajax.php"}};
+</script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-includes/js/wp-util.min.js?ver=5.3.2'></script>
+<script>
+var userProfileL10n = {"warn":"Tu contrase\u00f1a no se ha guardado.","warnWeak":"Confirma el uso de una contrase\u00f1a d\u00e9bil.","show":"Mostrar","hide":"Ocultar","cancel":"Cancelar","ariaShow":"Mostrar contrase\u00f1a","ariaHide":"Ocultar contrase\u00f1a"};
+</script>
+<script src='http://localhost/https:/mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
+=======
 				<p id="backtoblog"><a href="/https://mauxpl.github.io/MADESIGNART-PROJET-2020/">
 		&larr; Volver a MADESIGIN		</a></p>
 			</div>
@@ -77,6 +117,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/MADESIGNART-PROJET-2020\/wp-admin\/admin
 var userProfileL10n = {"warn":"Tu contrase\u00f1a no se ha guardado.","warnWeak":"Confirma el uso de una contrase\u00f1a d\u00e9bil.","show":"Mostrar","hide":"Ocultar","cancel":"Cancelar","ariaShow":"Mostrar contrase\u00f1a","ariaHide":"Ocultar contrase\u00f1a"};
 </script>
 <script src='/https://mauxpl.github.io/MADESIGNART-PROJET-2020/wp-admin/js/user-profile.min.js?ver=5.3.2'></script>
+>>>>>>> e0130585e53209f885b18e72ee7f30c3014065cd
 	<script>
 	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
 	</script>
